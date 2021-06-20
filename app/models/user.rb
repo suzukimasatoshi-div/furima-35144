9d2d7030b5488ce860e4ec6567ef361bb17db32b
suzukimasatoshi-div <life6pride-h.m.t.r.hi.hi.hi@ezweb.ne.jp>
2021-06-21 05:15:31 +0900

    正規表現リファクタリング

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,17 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :password, format: { with: /(?=.*?[a-zA-Z])(?=.*?\d)[a-zA-Z\d]{6,}/ }
-    validates :first_name_full_width, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :first_name_kana_full_width, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :last_name_full_width, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :last_name_kana_full_width, format: { with: /\A[ァ-ヶー－]+\z/ }
+    
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :first_name_full_width
+      validates :last_name_full_width
+    end 
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana_full_width 
+      validates :last_name_kana_full_width
+    end
+
     validates :birthday
   end
 end
