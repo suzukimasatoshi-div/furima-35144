bf1914015f345e04895f4d3e9d55ba293f0e72b5
suzukimasatoshi-div <life6pride-h.m.t.r.hi.hi.hi@ezweb.ne.jp>
2021-06-27 06:22:00 +0900

    バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,15 +1,25 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
 
   def index
+    #@items = Item.all.order('created_at DESC')
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def item_params
-    params.require(:item).permit(:image)
+    params.require(:item).permit(:image, :product_name, :product_descriiption, :category_id, :product_condition_id, :price, :shipping_charge_id, :shipping_charge_id, :delivery_area_id, :days_to_delivery_id ).merge(user_id: current_user.id)
   end
-end
+
+end 
