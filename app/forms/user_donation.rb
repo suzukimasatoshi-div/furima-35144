1ecaa05f55ae32df9feed46e86dc875889ee41c8
suzukimasatoshi-div <life6pride-h.m.t.r.hi.hi.hi@ezweb.ne.jp>
2021-07-03 12:22:52 +0900

    差分を出すため

--- app/forms/user_donation.rb
+++ app/forms/user_donation.rb
@@ -1,4 +1,4 @@
 class UserDonation
-  include ActiveModel: :Model
-  attr_accessor :name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price
+  #include ActiveModel: :Model
+  #attr_accessor :name, :name_reading, :nickname, :postal_code, :prefecture, :city, :house_number, :building_name, :price
 end
