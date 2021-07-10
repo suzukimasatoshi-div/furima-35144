db05a2f1fc695eaf3978f183968f53ddadf7ff01
suzukimasatoshi-div <life6pride-h.m.t.r.hi.hi.hi@ezweb.ne.jp>
2021-07-10 17:50:49 +0900

    商品購入機能レビュー内容修正

--- app/controllers/purehases_controller.rb
+++ app/controllers/purehases_controller.rb
@@ -1,14 +1,15 @@
 class PurehasesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :set_purehase, only: [:index, :create]
+
 
   def index
     @order = PurehaseShipping.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @order = PurehaseShipping.new(order_params)
-    @item = Item.find(params[:item_id])
     if @order.valid?
       Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
       Payjp::Charge.create(
@@ -29,4 +30,13 @@ class PurehasesController < ApplicationController
     params.require(:purehase_shipping).permit(:postal_code,:delivery_area_id,:municipality,:address,:building_name,:phone_number ).merge(user_id:current_user.id,item_id:params[:item_id],token:params[:token])
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def set_purehase
+    if @item.purehase.present?
+      redirect_to root_path
+    end
+  end
 end
