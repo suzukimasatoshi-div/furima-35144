fa6f4182e65458ec1516933eaeb70b7f0a06cd80
suzukimasatoshi-div <life6pride-h.m.t.r.hi.hi.hi@ezweb.ne.jp>
2021-07-12 05:24:44 +0900

    追加分修正

--- app/models/purehase_shipping.rb
+++ app/models/purehase_shipping.rb
@@ -10,12 +10,12 @@ class PurehaseShipping
     validates :municipality
     validates :address
     validates :phone_number, length: {maximum: 11 }
-    validates :phone_number, format: { with: /\A[0-9]+\z/ }
     validates :user_id
     validates :item_id
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
   end
 
+    validates :phone_number, format: { with: /\A[0-9]+\z/ }
     
   def save
     purehase = Purehase.create(user_id: user_id, item_id: item_id)
