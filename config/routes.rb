e2c8f279d99f0115448d3627f2d49f908a200154
suzukimasatoshi-div <life6pride-h.m.t.r.hi.hi.hi@ezweb.ne.jp>
2021-07-28 06:25:45 +0900

    コメント機能コントローラー、モデル導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'messages/new'
   devise_for :users
   root 'items#index'
-
+  resources :messages, only: [:create]
   resources :items do
     resources :purehases, only: [ :index, :create ]
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_03_060837) do
+ActiveRecord::Schema.define(version: 2021_07_27_211111) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -46,6 +46,12 @@ ActiveRecord::Schema.define(version: 2021_07_03_060837) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "messages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "purehases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
